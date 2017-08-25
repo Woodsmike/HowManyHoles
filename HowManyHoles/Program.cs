c3dd03ff9e6d9af675c80703a06f0637322f30f1
Woodsmike <Woodsmike@users.noreply.github.com>
2017-08-25 12:36:40 -0400

    Initial commit

---
+++ HowManyHoles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowManyHoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("Numbers.txt");
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}

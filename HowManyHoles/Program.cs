8f87af0f5cedcfe66a6ea26c46b640d2b2ef969f
Woodsmike <Woodsmike@users.noreply.github.com>
2017-08-25 19:59:00 -0400

    finished project

--- HowManyHoles/Program.cs
+++ HowManyHoles/Program.cs
@@ -11,8 +11,55 @@ namespace HowManyHoles
     {
         static void Main(string[] args)
         {
-            StreamReader reader = new StreamReader("Numbers.txt");
-            Console.WriteLine(reader.ReadToEnd());
+            using (StreamReader reader = new StreamReader("Numbers.txt"))
+            {
+
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int holeCount = 0;
+                    //List<int> numListr = new List<int>(int.Parse(line));
+                    //int[] numbers = line.Split().Select(str => int.Parse(str)).ToArray();
+                    //string [] numList = line.Split().ToArray();
+                    //List<int> numList = line.Split().Select(Int32.Parse).ToList();
+                    int [] numbers = line.Select(c => c - '0').ToArray();
+                    //Console.WriteLine(numbers[1]);
+
+                    for(int i = 0; i <numbers.Length; i++)
+                    {
+                      
+                        if (numbers[i] == 4)
+                        {
+                            holeCount += 1;
+                        }
+                        else if (numbers[i] == 6)
+                        {
+                            holeCount += 1;
+                        }
+                        else if (numbers[i] == 8)
+                        {
+                            holeCount += 2;
+                        }
+                        else if (numbers[i] == 9)
+                        {
+                            holeCount += 1;
+                        }
+                        else if (numbers[i] == 0)
+                        {
+                            holeCount += 1;
+                        }
+
+                        else
+                        {
+                            continue;
+                        }
+                       
+                    }
+                    Console.WriteLine(holeCount);                  
+
+                }
+            }
         }
     }
 }
